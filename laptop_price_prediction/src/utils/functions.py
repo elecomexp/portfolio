8c150955b410d7d228010efb94d8b10ce91b20db
Lander <lander.combarro@outlook.com>
2024-09-20 19:54:54 +0200

    laptop_price_prediction creation

---
+++ laptop_price_prediction/src/utils/functions.py
@@ -0,0 +1,31 @@
+def kaggle_checker(df_to_submit, path='./submissions/submission_lander.csv'):
+    """
+    Esta función se asegura de que tu submission tenga la forma requerida por Kaggle.
+    
+    Si es así, se guardará el dataframe en un `csv` y estará listo para subir a Kaggle.
+    
+    Si no, LEE EL MENSAJE Y HAZLE CASO.
+    
+    Si aún no:
+    - apaga tu ordenador, 
+    - date una vuelta, 
+    - enciendelo otra vez, 
+    - abre este notebook y 
+    - leelo todo de nuevo. 
+    Todos nos merecemos una segunda oportunidad. También tú.
+    """
+    if df_to_submit.shape == sample.shape:
+        if df_to_submit.columns.all() == sample.columns.all():
+            if df_to_submit.laptop_ID.all() == sample.laptop_ID.all():
+                print("You're ready to submit!")
+                submission.to_csv(path, index=False) #muy importante el index = False
+                urllib.request.urlretrieve("https://www.mihaileric.com/static/evaluation-meme-e0a350f278a36346e6d46b139b1d0da0-ed51e.jpg", "../img/gfg.png")     
+                img = Image.open("gfg.png")
+                img.show()   
+            else:
+                print("Check the ids and try again")
+        else:
+            print("Check the names of the columns and try again")
+    else:
+        print("Check the number of rows and/or columns and try again")
+        print("\nMensaje secreto de Iván y Manuel: No me puedo creer que después de todo este notebook hayas hecho algún cambio en las filas de `laptops_test.csv`. Lloramos.")
